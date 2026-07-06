4f6ffce59c8fcdd8b8e85b31be80c0fa568f36f4
Bezawityfru2 <Bezawityfru21@gmail.com>
2026-07-06 04:03:09 +0300

    update frontend api url for productio

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const instance =  axios.create({
-  baseURL: "http://localhost:5500/api",
+const instance = axios.create({
+  baseURL: "https://api-node-payment-4djf.onrender.com/api",
 });
 
 export default instance;
